fb128c5c668d6b92a33443c0fa42e27139851355
Daniel Barreto <danyel.rbarreto@gmail.com>
2024-05-09 10:39:49 +0100

    More Complex

--- basics.ts
+++ basics.ts
@@ -1,3 +1,6 @@
+//primitives
+
+
 let age: number;
 age = 12;
 
@@ -7,3 +10,19 @@ userName = 'Max';
 
 let isInstructor: boolean;
 isInstructor = true;
+
+//more complex
+
+let hobbies: string[];
+
+hobbies =['sports', 'cookies'];
+
+let person: {
+    name: string;
+    ag: number;
+}
+
+let people:{
+    name: string
+    age: number;
+}[];
